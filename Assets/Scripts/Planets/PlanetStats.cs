4f9875d4c8270a6e6b2600517a1547089b26b4a0
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-11-21 15:18:44 +0100

    Added Planets Scriptable Objects

---
+++ Assets/Scripts/Planets/PlanetStats.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Planet
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Planet/Stats", fileName = "PlanetStats")]
+    public class PlanetStats : ScriptableObject
+    {
+        [Header("Planet's Transform")] 
+        [SerializeField] private Vector3 position;
+
+        [SerializeField] private float scale;
+
+        [Header("Physics")]
+        [SerializeField] private float mass;
+
+        [SerializeField] private float gravityFieldScale;
+
+        [Header("GameObjects")] 
+        public GameObject planet;
+
+        public GameObject gravityField;
+
+        public Vector3 Position => position;
+        public float Scale => scale;
+        
+        public float Mass => mass;
+        public float GravityFieldScale => gravityFieldScale;
+    }
+    
+}
---
+++ Assets/Scripts/Stage/PlanetsPlacer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Planet;
+using UnityEngine;
+
+
+namespace Stage
+{
+    public class PlanetsPlacer : MonoBehaviour
+    {
+        [SerializeField] private List<PlanetStats> planets;
+    }    
+}
+
