a5c624128caa40ece79cb63bbe374c554dc57b25
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-24 14:49:54 +0100

    Refactored EnginesFireScale.cs

--- Assets/Scripts/Player/EnginesFireScale.cs
+++ Assets/Scripts/Player/EnginesFireScale.cs
@@ -1,21 +1,33 @@
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Player
 {
     public class EnginesFireScale : MonoBehaviour
     {
         [SerializeField] private float minX;
+        [SerializeField] private float minY;
         [SerializeField] private float minZ;
 
-        private void FixedUpdate()
+        private Vector3 _scaleMultiplier;
+
+        private void Awake() => _scaleMultiplier = transform.localScale;
+        
+        private void FixedUpdate() => SetScale();
+        
+        private void SetScale() => transform.localScale = GenerateScaleValues();
+       
+        private Vector3 GenerateScaleValues()
         {
-            var scaleX = Random.Range(minX, 1);
-            var scaleZ = Random.Range(minZ, 1);
+            var scaleX = SetScaleValue(minX, _scaleMultiplier.x);
+            var scaleY = SetScaleValue(minY, _scaleMultiplier.y);
+            var scaleZ = SetScaleValue(minZ, _scaleMultiplier.z);
             
-            transform.localScale = new Vector3(scaleX, 1, scaleZ);
+            return new Vector3(scaleX, scaleY, scaleZ);
         }
+
+        private float SetScaleValue(float min, float multiplier)
+            => Random.Range(min, 1) * multiplier;
+        
     }    
 }
 
